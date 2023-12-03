975a5e4d4b885d01b1e7e15e8b25ebfa6668a570
Erick Ivan Elizalde <ielizalde185@gmail.com>
2023-12-03 14:50:06 -0600

    borrado

--- app/src/main/java/com/example/proyectorovilesborrador/AyudaActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.proyectorovilesborrador;
-
-import android.app.Activity;
-
-public class AyudaActivity extends Activity {
-}
--- app/src/main/java/com/example/proyectorovilesborrador/Usuario.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyectorovilesborrador
-
-import java.io.Serializable
-
-data class Usuario (val nombre: String, val correo: String, val password: String, val telefono: String): Serializable{
-
-}
