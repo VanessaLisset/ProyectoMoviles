71fef4caafb84a9484f3d58788a6276537ef27a6
XxNightmare <Rafael.Isai.Otero.Cabrero@gmail.com>
2023-12-03 15:53:07 -0600

    .

--- ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/Detalle/Detalleragment.kt
+++ ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/Detalle/Detalleragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,42 +14,52 @@ import com.example.proyectofinal_moviles.databinding.ActivityDetalleBinding
 import com.example.proyectofinal_moviles.producto
 
 class Detalleragment : Fragment() {
-
     private var _binding: ActivityDetalleBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
+
         _binding = ActivityDetalleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        arguments?.let { bundle ->
-            val productoNombre = bundle.getString("productoNombre", "")
-            val productoMarca = bundle.getString("productoMarca", "")
-
-            binding.txtproductName.text = productoNombre
-            binding.txtproductDetails.text = productoMarca
+        // Recibir el ID del producto
+        val productoIds = arguments?.getString("claveProducto")
+        var productoId = 0;
+        try {
+            productoId = productoIds!!.toInt()
+        } catch (ex: NumberFormatException) {
+            println("The given string is non-numeric")
         }
+
+        // Aquí simplemente estamos estableciendo textos de ejemplo
+        val productoEncontrado = obtenerProducto(productoId)
+
+        binding.txtproductName.text = productoEncontrado?.nombre
+        binding.txtproductDetails.text = productoEncontrado?.modelo
 
         return root
     }
-
-
-    private fun navegarADetalleFragment(producto: producto) {
-        val bundle = Bundle().apply {
-            putInt("productoId", producto.id)
-            putString("productoNombre", producto.nombre)
-            putString("productoMarca", producto.marca)
-        }
-
-        findNavController().navigate(R.id.detalleragment, bundle)
-    }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun obtenerProducto(id_producto: Int): producto? {
+        val productos = listOf(
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 1),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 2),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 3),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 4),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 5),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 6),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 7),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 8),
+            producto(R.drawable.prod1, "Compactadora", "Marca X", "Modelo Y", "$550", 9),
+        )
+        return productos.find { it.id == id_producto }
+    }
+
 }
