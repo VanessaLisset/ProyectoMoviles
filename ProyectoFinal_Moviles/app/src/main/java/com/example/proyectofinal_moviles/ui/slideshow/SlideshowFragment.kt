456bcbe5321b9cddaa20ebcb4b872e367e49eb2c
XxNightmare <Rafael.Isai.Otero.Cabrero@gmail.com>
2023-12-03 17:03:35 -0600

    Mi perfil y ayuda

--- ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/slideshow/SlideshowFragment.kt
+++ ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/slideshow/SlideshowFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.proyectofinal_moviles.Registro
 import com.example.proyectofinal_moviles.databinding.FragmentSlideshowBinding
 
 class SlideshowFragment : Fragment() {
@@ -28,10 +30,17 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val ultimoUsuario = Registro.usuariosRegistrados.lastOrNull()
+        ultimoUsuario?.let {
+            binding.txtMostrarNombre.text = it.usuario
+            binding.txtMostrarCorreo.text = it.correo
+            binding.txtMostrarTelefono.text = it.telefono
+            binding.txtMostrarContrasena.text = it.password
         }
+        binding.btnSaludarPerfil.setOnClickListener {
+            Toast.makeText(context, "Saludos", Toast.LENGTH_SHORT).show()
+        }
+
         return root
     }
 
