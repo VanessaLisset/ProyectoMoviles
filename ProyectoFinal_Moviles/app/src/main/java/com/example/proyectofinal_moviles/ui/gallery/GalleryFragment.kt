5498a10c74da7efa46f9280128823c7d33009ba0
vane <vf65163@gmail.com>
2023-12-03 13:46:49 -0600

    1.2
    dios no

--- ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/gallery/GalleryFragment.kt
+++ ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ui/gallery/GalleryFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.proyectofinal_moviles.Registro
+import com.example.proyectofinal_moviles.ui.gallery.GalleryViewModel
 import com.example.proyectofinal_moviles.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
