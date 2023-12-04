068c442636908c5bcf5ca317675d526e39100128
XxNightmare <Rafael.Isai.Otero.Cabrero@gmail.com>
2023-12-03 23:46:59 -0600

    .

--- ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ProductAdapter.kt
+++ ProyectoFinal_Moviles/app/src/main/java/com/example/proyectofinal_moviles/ProductAdapter.kt
@@ -3,18 +3,19 @@ package com.example.proyectofinal_moviles
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductAdapter(private val listaProductos: List<producto>, private val onClick: (producto) -> Unit) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
-
+class ProductAdapter(private val listaProductos: List<producto>, private val onClick: (producto) -> Unit, private val onFavoritoClick: (producto) -> Unit) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
     class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val productImage: ImageView = view.findViewById(R.id.product_image)
         val productName: TextView = view.findViewById(R.id.txtproduct_name)
         val productBrand: TextView = view.findViewById(R.id.txtproduct_details)
         val productModel: TextView = view.findViewById(R.id.product_details2)
         val productPrice: TextView = view.findViewById(R.id.product_price)
+        val btnFavorito: Button = view.findViewById(R.id.favorito)
 
         fun bind(producto: producto) {
             productImage.setImageResource(producto.imagen)
@@ -32,10 +33,8 @@ class ProductAdapter(private val listaProductos: List<producto>, private val onC
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val producto = listaProductos[position]
-        holder.bind(producto)
         holder.itemView.setOnClickListener { onClick(producto) }
+        holder.btnFavorito.setOnClickListener { onFavoritoClick(producto) }
     }
-
-
     override fun getItemCount() = listaProductos.size
 }
